61ac851a9efcb57c3a4e4fd2a9487d3751847d82
IBrickhouse <mrfurbyb@gmail.com>
2025-04-30 23:35:58 -0700

    Add client code and work out bugs. Works

--- server.py
+++ server.py
@@ -1,47 +1,62 @@
-import threading
 import socket
+import threading
 
-host = '127.0.0.1' # localhost
-port = 55555
+# Connection Data
+host = '127.0.0.1'
+port = 55556
 
+# Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
+# Lists For Clients and Their Nicknames
 clients = []
 nicknames = []
 
+# Sending Messages To All Connected Clients
 def broadcast(message):
     for client in clients:
         client.send(message)
 
+# Handling Messages From Clients
 def handle(client):
     while True:
         try:
+            # Broadcasting Messages
             message = client.recv(1024)
             broadcast(message)
         except:
+            # Removing And Closing Clients
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            broadcast('{} left!'.format(nickname).encode('ascii'))
             nicknames.remove(nickname)
             break
 
-def recieve():
+# Receiving / Listening Function
+def receive():
     while True:
-        clident, address = server.accept()
-        print(f"Connected with {str(address)}")
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
 
-        client.send("NICK".encode('ascii'))
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
 
-        print(f"Nickname of the client is {nickname}")
-        broadcast(f"{nickname} joined the chat!".encode('ascii'))
-        client.send('Connected to the server!'.encode('ascii'))
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
 
-        thread = threadng.Thread(target=handle, args=(client,))
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+print("The server is listening...")
+receive()
